ethereum/execution-spec-tests

6968: initial test and structure for 6968 tests

src/ethereum_test_forks/forks/forks.py
@@ -405,3 +405,17 @@ class Cancun(Shanghai):
         Starting at Cancun, payloads must have a parent beacon block root.
         """
         return True
+
+
+class Prauge(Cancun):
+    """
+    Cancun fork
+    """
+
+    @classmethod
+    def is_deployed(cls):
+        """
+        Flags that Cancun has not been deployed to mainnet; it is under active
+        development.
+        """
+        return False

numbergroup/execution-spec-tests-6968

src/ethereum_test_forks/forks/forks.py
@@ -405,3 +405,17 @@ class Cancun(Shanghai):
         Starting at Cancun, payloads must have a parent beacon block root.
         """
         return True
+
+
+class Prague(Cancun):
+    """
+    Prague fork
+    """
+
+    @classmethod
+    def is_deployed(cls):
+        """
+        Flags that Prague has not been deployed to mainnet; it is under active
+        development.
+        """
+        return False